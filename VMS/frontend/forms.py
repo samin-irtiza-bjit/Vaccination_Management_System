be64ce57e3e91104487ca3830751d93b419d1182
samin irtiza <samin.irtiza@bjitgroup.com>
2023-08-23 08:24:58 +0600

    Functioning Register Form

--- VMS/frontend/forms.py
+++ VMS/frontend/forms.py
@@ -1,25 +1,20 @@
 from django import forms
 from .models import User,Vaccine,Vaccination
 from datetime import date, timedelta
-
-# class VaccineForm(forms.ModelForm):
-#     class Meta:
-#         model = Vaccine
-#         fields = ['vaccine_name']
+from django import forms
+from .models import User, Vaccine
 
 class UserForm(forms.ModelForm):
     name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     address = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     nid = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-
     class Meta:
         model = User
         fields = ['name', 'address', 'nid']
 
-class VaccinationForm(forms.ModelForm):
-    vaccine = forms.ModelChoiceField(queryset=Vaccine.objects.all().order_by('vaccine_name'), widget=forms.Select(attrs={'class': 'form-select'}))
-    # vaccination_date = forms.DateField(initial=date.today() + timedelta(days=7), input_formats=['%m-%d-%Y'])
-    class Meta:
-        model = Vaccination
-        fields = ("user","vaccine","vaccination_date")
-
+class VaccinationForm(forms.Form):
+    user = forms.ModelChoiceField(queryset=User.objects.all())
+    vaccine = forms.ModelChoiceField(queryset=Vaccine.objects.all())
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields['vaccine'].queryset = Vaccine.objects.all()
--- VMS/frontend/views.py
+++ VMS/frontend/views.py
@@ -1,32 +1,33 @@
 from django.shortcuts import render
+from django.utils import timezone
 from datetime import date, timedelta
 from .forms import UserForm,VaccinationForm
+from .models import Vaccine, Vaccination
 
 # Create your views here.
 def home(request):
     return render(request,'home.html',{})
 
 def login(request):
-    if request.method=='POST':
-        request.POST = request.POST.copy()
+    if request.method == 'POST':
         user_form = UserForm(request.POST)
-        request.POST['vaccination_date'] = date.today() + timedelta(days=7)
         vaccination_form = VaccinationForm(request.POST)
-        if user_form.is_valid():
-            user = user_form.save(commit=True)
-            print(user)
-            print(request.POST)
-            # print(vaccination_form.data) # to see the form data
-            # print(vaccination_form.errors) # to see the form errors
-            # vaccination = vaccination_form.save(commit=False)
-            vaccination_form.user = user
-            # try:
-            #     vaccination_form.is_valid() # to validate the form data
-            vaccination_form.save() # to save the form data
-            # except ValueError as e:
-            #     print(e) # to see the error message
-            # redirect('home')
+        
+        # if user_form.is_valid() and vaccination_form.is_valid():
+        user = user_form.save()  # Save the new User entry
+        vaccination_date = timezone.now() + timedelta(days=7)
+        vaccine_id = vaccination_form.data['vaccine']
+        vaccine = Vaccine.objects.get(id=vaccine_id)
+        vaccination = Vaccination(user=user, vaccine=vaccine, vaccination_date=vaccination_date)
+        vaccination.save()  # Save the new Vaccination entry
+                
     else:
         user_form = UserForm()
         vaccination_form = VaccinationForm()
+    
+    context = {
+        'user_form': user_form,
+        'vaccination_form': vaccination_form,
+    }
+
     return render(request, 'login.html', {'user_form': user_form, 'vaccination_form': vaccination_form})
